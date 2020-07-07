98dacd551f0bda714cae97b88ec9f8d513d62e35
Jubida <berajaalan@gmail.com>
2020-07-07 20:03:30 -0300

    Scripts "MainMenu" e "OptionsMenu" adicionados
    O script "MainMenu" serve para guardar funções do menu principal que são necessárias (ex: quit fecha o jogo) e o script "OptionsMenu" faz o mesmo para o menu de opções

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+    public void Quit() {
+        Application.Quit();
+        Debug.Log("O jogo só fecha na build, no editor essa mensagem é enviada");
+    }
+
+}
---
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsMenu : MonoBehaviour {
+
+    #region Video Settings
+
+    public void ScreenResolution() {
+
+    }
+
+    public void Fullscreen(bool isFullscreen) {
+
+    }
+
+    #endregion
+
+    #region Sound Settings
+
+    public void ChangeMasterVolume(float newVolume) {
+
+    }
+
+    public void ChangeSFXVolume(float newVolume) {
+
+    }
+
+    public void ChangeMusicVolume(float newVolume) {
+
+    }
+
+    #endregion
+
+    #region Controls Settings
+
+    public void SetControlsToDefault() {
+
+    }
+
+    #endregion
+
+    #region Accessibility Setiings
+
+    public void ColorBlindMode() {
+
+    }
+
+    #endregion
+}
