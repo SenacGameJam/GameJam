b7f11ae7e84d2599faafd5352749aa5623dcdaba
Jubida <berajaalan@gmail.com>
2020-07-09 21:16:24 -0300

    Revert "Revert "Menu melhorado de diversas formas""
    This reverts commit 6545cb42463aa84f4ffd58b0b23bec3218fccf93.

--- Assets/Scripts/Menu/SettingsMenu.cs
+++ Assets/Scripts/Menu/SettingsMenu.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class OptionsMenu : MonoBehaviour {
+public class SettingsMenu : MonoBehaviour {
 
     #region Video Settings
 
